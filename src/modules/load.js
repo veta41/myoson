36555d23610911f001f0c16d1c654033f6814fcc
Svitlana Chudesa <chsveta34@gmail.com>
2021-12-19 14:50:48 +0200

    переименовывает старый файл

---
+++ src/modules/load.js
@@ -0,0 +1,13 @@
+import getData from "./getData"
+import renderGoods from "./renderGoods"
+
+
+
+const load = () => {
+     getData().then((data) => {
+        renderGoods(data);
+    })
+    
+   
+}
+export default load

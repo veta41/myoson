28edd65d1c52aefb2fcb4522fd09f9b5b59de9cb
Svitlana Chudesa <chsveta34@gmail.com>
2021-12-19 14:54:28 +0200

    добавляет сортировку и фильтр по товару на странице

---
+++ src/modules/filters.js
@@ -0,0 +1,13 @@
+export const searchFilter = (goods, value) => {
+  return goods.filter((goodsItem) => {
+    return goodsItem.title.toLowerCase().includes(value.toLowerCase())
+  })
+
+}
+
+export const categoryFilter = (goods, value) => {
+  return goods.filter((goodsItem) => {
+    return goodsItem.category === value
+  })
+
+}

357e417dfe053d420c777b52c80d91618ad00c04
Svitlana Chudesa <chsveta34@gmail.com>
2021-12-20 18:59:58 +0200

    добавляет функционал фильтра по цене

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -11,3 +11,21 @@ export const categoryFilter = (goods, value) => {
   })
 
 }
+
+// min price search
+
+export const minSizeFilter = (goods, minValue) => {
+
+  return goods.filter((goodsItem) => {
+    return goodsItem.price >= minValue;
+  });
+};
+
+//  max price search
+
+export const maxSizeFilter = (goods, maxValue) => {
+
+  return goods.filter((goodsItem) => {
+    return goodsItem.price <= maxValue;
+  });
+};
---
+++ src/modules/size.js
@@ -0,0 +1,13 @@
+import getData from "./getData";
+import renderGoods from "./renderGoods";
+import { minSizeFilter, maxSizeFilter } from "./filters";
+
+const size = () => {
+
+  const fromSize = document.querySelector('#min');
+  const toSize = document.querySelector('#max');
+  const checkboxButton = document.querySelector("#discount-checkbox-test");
+
+}
+
+export default size
